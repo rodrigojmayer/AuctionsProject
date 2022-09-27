31a9e0f87979d91cae6f82855ad15b27604062d6
Rodrigo <rodrigojmayer@gmail.com>
2022-09-27 15:13:38 +0200

    dropdown user logged

---
+++ auctions/static/auctions/inbox.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    // alert("Its working!")
+    // document.getElementById("user-logged").onmouseover=function(){
+    //     document.getElementById("myDropdownUser").classList.add('show');
+    //     document.getElementById("myDropdownUser").classList.remove('hide');
+
+    // };
+    // document.getElementById("user-logged").onmouseout=function(){
+    //     document.getElementById("myDropdownUser").classList.add('hide');
+    //     document.getElementById("myDropdownUser").classList.remove('show');
+    // };
+})
