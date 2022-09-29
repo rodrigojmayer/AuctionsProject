ced4b1fe3a84d1ff815712a3352151da1bbca3d0
Rodrigo <rodrigojmayer@gmail.com>
2022-09-29 11:24:30 +0200

    improvements watchlist and passwords validations

--- commerce/settings.py
+++ commerce/settings.py
@@ -86,20 +86,22 @@ AUTH_USER_MODEL = 'auctions.User'
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+
+#     Comentado para poder cambiar las contraseñas sin validaciones molestas
+# # AUTH_PASSWORD_VALIDATORS = [
+# #     {
+# #         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+# #     },
+# #     {
+# #         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+# #     },
+# #     {
+# #         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+# #     },
+# #     {
+# #         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+# #     },
+# # ]
 
 
 # Internationalization
