6ded09d71e4adb0642deda49186cc59058d93618
Rodrigo <rodrigojmayer@gmail.com>
2022-10-04 19:30:16 +0200

    adding modal to listing page place bid 2

--- auctions/static/auctions/react.jsx
+++ auctions/static/auctions/react.jsx
@@ -71,39 +71,25 @@ function Modal(){
 
     // console.log(modal.getAttribute('value'))
     
-    if(modal.getAttribute('value') == "create_listing"){
-        return(
-            <div class="modal-alert hide" >
-                <div class="modal-body ">
-                    <h5 id="modal_subtitle"></h5>
-                    <p id="modal_text"></p>
-                    <div class="flex-horizontal">
-                        <button id="modal_back"  class="btn btn-primary" type="button" value="Back" >Back</button>
-                        <input id="modal_save" onclick="submitButtonClick(event)" class="btn btn-primary" type="submit" value="Save" name="_save" />
-                    </div>
+    return(
+        <div class="modal-alert hide" >
+            <div class="modal-body ">
+                <h5 id="modal_subtitle"></h5>
+                <p id="modal_text"></p>
+                <div class="flex-horizontal">
+                    <button id="modal_back"  class="btn btn-primary" type="button" value="Back" >Back</button>
+                    <input id="modal_save" onclick="submitButtonClick(event)" class="btn btn-primary" type="submit" value="Save" name="_save" />
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 
     // else if(modal.getAttribute('value') == "listing_page"){
 
         // return()
     // }
-    if(modal.getAttribute('value') == "listing_page"){
-        return(
-            <div class="modal-alert hide" >
-                <div class="modal-body ">
-                    <h5 id="modal_subtitle"></h5>
-                    <p id="modal_text"></p>
-                    <div class="flex-horizontal">
-                        <button id="modal_back"  class="btn btn-primary" type="button" value="Back" >Back</button>
-                        <input id="modal_save" onclick="submitButtonClick(event)" class="btn btn-primary" type="submit" value="Save" name="_save" />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+    // if(modal.getAttribute('value') == "listing_page"){
+    // }
 
 
 }
@@ -130,78 +116,83 @@ function handleModal() {
     let modal_back = document.getElementById("modal_back");
     let modal_save = document.getElementById("modal_save");
     
-    let data_article = document.getElementById("data_article");
-    let data_description = document.getElementById("data_description");
-    let data_initial_price = document.getElementById("data_initial_price");
-    let data_auction_category = document.getElementById("data_auction_category");
-    let data_image = document.getElementById("image");
-    let modal_answer_text = ""
-    // data_article.value=1
-    // data_description.value=1
-    // data_initial_price.value=1
-    // data_image.value=1
+    console.log("modal value")
+    console.log(modal.getAttribute('value'))
+    if(modal.getAttribute('value') == "create_listing"){
+        let data_article = document.getElementById("data_article");
+        let data_description = document.getElementById("data_description");
+        let data_initial_price = document.getElementById("data_initial_price");
+        let data_auction_category = document.getElementById("data_auction_category");
+        let data_image = document.getElementById("image");
+        let modal_answer_text = ""
+        // data_article.value=1
+        // data_description.value=1
+        // data_initial_price.value=1
+        // data_image.value=1
 
-    
-    if(!data_article.value){
-        console.log("Article name missing")
-        modal_answer_text += "- Name</br>"
-    }
-    if(!data_description.value){
-        console.log("Description missing")
-        modal_answer_text += " - Description</br>"
-    }
-    if(!data_initial_price.value){
-        console.log("Price missing")
-        modal_answer_text += " - Price</br>"
-    }
-    if(!data_auction_category.value){
-        console.log("Category missing")
-        modal_answer_text += " - Category</br>"
-    }
-    if(!data_image.value){
-        console.log("Image missing")
-        modal_answer_text += " - Image</br>"
-    }
+        if(!data_article.value){
+            console.log("Article name missing")
+            modal_answer_text += "- Name</br>"
+        }
+        if(!data_description.value){
+            console.log("Description missing")
+            modal_answer_text += " - Description</br>"
+        }
+        if(!data_initial_price.value){
+            console.log("Price missing")
+            modal_answer_text += " - Price</br>"
+        }
+        if(!data_auction_category.value){
+            console.log("Category missing")
+            modal_answer_text += " - Category</br>"
+        }
+        if(!data_image.value){
+            console.log("Image missing")
+            modal_answer_text += " - Image</br>"
+        }
 
-    if(modal_answer_text){
-        modal_subtitle.innerHTML = "Missing data"
-        modal_text.innerHTML = `You need to enter: </br> ${modal_answer_text}`
-        rejected_modal()
-    }
-    else{
 
-        const formData = new FormData();
-        formData.append('name', data_article.value);
-        formData.append('csrfmiddlewaretoken', '{{ csrf_token }}');
-        // console.log(formData);
-        fetch(`/check_auction/`, {
-            method: 'POST',
-            body:formData,
-        })
-        .then(response => response.json())
-        .then(result => {
-            console.log(result.auction_repeated)
-            if(result.auction_repeated){
-                modal_subtitle.innerHTML = "Duplicate data"
-                modal_text.innerHTML = "There is already an active article with that name."
-                rejected_modal()
-            }
-            else{
-                modal_subtitle.innerHTML = "Post item"
-                modal_text.innerHTML = `Name: ${data_article.value}</br>
-                                        Description: ${data_description.value}</br>
-                                        Price: ${data_initial_price.value}</br>
-                                        Category: ${data_auction_category.value}</br>
-                                        Image: ${document.getElementById("glosaArchivos").textContent}</br>`
-                modal_save.classList.remove('hide');
-                modal_save.classList.add('show');
-                modal_save.onclick="";
-            }
+        if(modal_answer_text){
+            modal_subtitle.innerHTML = "Missing data"
+            modal_text.innerHTML = `You need to enter: </br> ${modal_answer_text}`
+            rejected_modal()
+        }
+        else{
 
-        })
-        .catch((error) => {
-            console.log(error)
-        });
+            const formData = new FormData();
+            if(data_article)
+                formData.append('name', data_article.value);
+            formData.append('csrfmiddlewaretoken', '{{ csrf_token }}');
+            // console.log(formData);
+            fetch(`/check_auction/`, {
+                method: 'POST',
+                body:formData,
+            })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result.auction_repeated)
+                if(result.auction_repeated){
+                    modal_subtitle.innerHTML = "Duplicate data"
+                    modal_text.innerHTML = "There is already an active article with that name."
+                    rejected_modal()
+                }
+                else{
+                    modal_subtitle.innerHTML = "Post item"
+                    modal_text.innerHTML = `Name: ${data_article.value}</br>
+                                            Description: ${data_description.value}</br>
+                                            Price: ${data_initial_price.value}</br>
+                                            Category: ${data_auction_category.value}</br>
+                                            Image: ${document.getElementById("glosaArchivos").textContent}</br>`
+                    modal_save.classList.remove('hide');
+                    modal_save.classList.add('show');
+                    modal_save.onclick="";
+                }
+
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+        }
     }
     modal_back.addEventListener("click", function() {
         modal_alert.classList.add('hide');
