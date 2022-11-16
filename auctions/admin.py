6753acce44659300539c1013e3d5ebf0dca3e12c
Rodrigo <rodrigojmayer@gmail.com>
2022-11-16 10:07:14 +0100

    user admin form

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import User, AuctionListings, Bids, Comments, Category, WatchLists
 
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(AuctionListings)
 admin.site.register(Bids)
 admin.site.register(Comments)
